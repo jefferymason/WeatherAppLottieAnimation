b674c43854bc7f9ff28af19831f9863d4dcf3ef0
jefferymason <jefferymason72@gmail.com>
2021-04-21 12:27:51 -0500

    Initial Commit

---
+++ WeatherAppLottieAnimation/WeatherAppLottieAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppLottieAnimationApp.swift
+//  WeatherAppLottieAnimation
+//
+//  Created by Jeff Mason on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppLottieAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

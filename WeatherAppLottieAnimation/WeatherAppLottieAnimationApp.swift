78f65154ed854b9c60b796e89f1185322531c898
jefferymason <52844021+jefferymason@users.noreply.github.com>
2021-04-21 12:31:47 -0500

    Update WeatherAppLottieAnimationApp.swift

--- WeatherAppLottieAnimation/WeatherAppLottieAnimationApp.swift
+++ WeatherAppLottieAnimation/WeatherAppLottieAnimationApp.swift
@@ -1,17 +1,46 @@
-//
-//  WeatherAppLottieAnimationApp.swift
-//  WeatherAppLottieAnimation
-//
-//  Created by Jeff Mason on 4/21/21.
-//
+// Copy this....
 
-import SwiftUI
-
-@main
-struct WeatherAppLottieAnimationApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+func getWeatherIconFor(icon: String) -> Image {
+      switch icon {
+      case "01d" :
+          return Image(systemName: "sun.max.fill")
+      case "01n" :
+          return Image(systemName: "moon.fill")
+      case "02d" :
+          return Image(systemName: "cloud.sun.fill")
+      case "02n" :
+          return Image(systemName: "cloud.moon.fill")
+      case "03d" :
+          return Image(systemName: "cloud.fill")
+      case "03n" :
+          return Image(systemName: "cloud.fill")
+      case "04d" :
+          return Image(systemName: "cloud.fill")
+      case "04n" :
+          return Image(systemName: "cloud.fill")
+      case "09d" :
+          return Image(systemName: "cloud.drizzle.fill")
+      case "09n" :
+          return Image(systemName: "cloud.drizzle.fill")
+      case "10d" :
+          return Image(systemName: "cloud.heavyrain.fill")
+      case "10n" :
+          return Image(systemName: "cloud.heavyrain.fill")
+      case "11d" :
+          return Image(systemName: "cloud.bolt.fill")
+      case "11n" :
+          return Image(systemName: "cloud.bolt.fill")
+      case "13d" :
+          return Image(systemName: "cloud.snow.fill")
+      case "13n" :
+          return Image(systemName: "cloud.snow.fill")
+      case "50d" :
+          return Image(systemName: "cloud.fog.fill")
+      case "50n" :
+          return Image(systemName: "cloud.fog.full")
+      default:
+          return Image (systemName: "sun.max.fill")
+          
+      }
+  }
 }
